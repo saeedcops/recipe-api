e44bf18ba7bf3de51cb7ca227d67ea9e027fa399
saeedcops <saeed_alaky@hotmail.com>
2020-07-14 18:51:40 +0200

    configured postgres db

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
-
 from core import models
 # Register your models here.
+
 
 class UserAdmin(BaseUserAdmin):
     ordering=['id']
